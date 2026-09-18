d14f15dee56dac0b1c216a8a91964bac050617ca
muftohiddin318 <muftohiddin318@gmail.com>
2026-09-19 00:21:53 +0500

    Register web search tool

--- src/core/tools/registry.ts
+++ src/core/tools/registry.ts
@@ -1,8 +1,9 @@
 import { builtinTools } from "./builtins.js";
+import { webTools } from "./web.js";
 import type { ToolDefinition } from "./types.js";
 
 const tools = new Map<string, ToolDefinition<any, any>>();
-for (const tool of builtinTools) tools.set(tool.name, tool);
+for (const tool of [...builtinTools, ...webTools]) tools.set(tool.name, tool);
 
 export function listTools() {
   return [...tools.values()].map(({ execute, validateArgs, ...definition }) => definition);
