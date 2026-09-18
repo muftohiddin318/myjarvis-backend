93d18f3354684f41941d58783fe06b421e86cba0
muftohiddin318 <muftohiddin318@gmail.com>
2026-09-19 00:08:41 +0500

    Fix heterogeneous tool registry typing

--- src/core/tools/registry.ts
+++ src/core/tools/registry.ts
@@ -1,11 +1,11 @@
 import { builtinTools } from "./builtins.js";
 import type { ToolDefinition } from "./types.js";
 
-const tools = new Map<string, ToolDefinition>();
+const tools = new Map<string, ToolDefinition<any, any>>();
 for (const tool of builtinTools) tools.set(tool.name, tool);
 
 export function listTools() {
-  return [...tools.values()].map(({ execute, ...definition }) => definition);
+  return [...tools.values()].map(({ execute, validateArgs, ...definition }) => definition);
 }
 
 export function getTool(name: string) {
