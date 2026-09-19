fe1505ce0de78b3129dbaf2c5c0c7cbc4da3da73
muftohiddin318 <muftohiddin318@gmail.com>
2026-09-19 13:19:30 +0500

    Register Telegram sending tool

--- src/core/tools/registry.ts
+++ src/core/tools/registry.ts
@@ -1,9 +1,10 @@
 import { builtinTools } from "./builtins.js";
 import { webTools } from "./web.js";
+import { telegramTools } from "./telegram.js";
 import type { ToolDefinition } from "./types.js";
 
 const tools = new Map<string, ToolDefinition<any, any>>();
-for (const tool of [...builtinTools, ...webTools]) tools.set(tool.name, tool);
+for (const tool of [...builtinTools, ...webTools, ...telegramTools]) tools.set(tool.name, tool);
 
 export function listTools() {
   return [...tools.values()].map(({ execute, validateArgs, ...definition }) => definition);
