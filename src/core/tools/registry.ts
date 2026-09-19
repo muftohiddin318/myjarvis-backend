e01eece68ff41878cba9db063d1b488389234191
muftohiddin318 <muftohiddin318@gmail.com>
2026-09-19 13:41:11 +0500

    Register JARVIS Telegram bot send tool

--- src/core/tools/registry.ts
+++ src/core/tools/registry.ts
@@ -1,10 +1,11 @@
 import { builtinTools } from "./builtins.js";
 import { webTools } from "./web.js";
 import { telegramTools } from "./telegram.js";
+import { botTelegramTools } from "./botTelegram.js";
 import type { ToolDefinition } from "./types.js";
 
 const tools = new Map<string, ToolDefinition<any, any>>();
-for (const tool of [...builtinTools, ...webTools, ...telegramTools]) tools.set(tool.name, tool);
+for (const tool of [...builtinTools, ...webTools, ...telegramTools, ...botTelegramTools]) tools.set(tool.name, tool);
 
 export function listTools() {
   return [...tools.values()].map(({ execute, validateArgs, ...definition }) => definition);
